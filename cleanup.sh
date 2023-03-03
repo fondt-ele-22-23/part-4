b3410fd53c377ec5a934cebf09675a64921139ed
Michele Lombardi <michele.lombardi2@unibo.it>
2023-03-03 14:58:59 +0100

    Slightly better cleanup file

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,9 @@
 
 if [ `git branch --show-current` = "main" ]; then
     rm *-sol.ipynb
+    rm *\ -\ sol.ipynb
     rm pdfs/*-sol.pdf
+    rm pdfs/*\ -\ sol.pdf
     rm docker-compose.yml
     rm Dockerfile
     rm print-pdf.sh
